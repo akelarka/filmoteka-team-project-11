04c1b14d969cf85922f4a8960da81a89b27e4089
D. Akelar <108392983+akelarka@users.noreply.github.com>
2023-01-06 17:23:54 +0200

    ww

--- src/js/renderMovieModal.js
+++ src/js/renderMovieModal.js
@@ -68,8 +68,4 @@ export function renderMovieModal({
   </div>
 </div>`;
   divModal.firstElementChild.insertAdjacentHTML('afterend', markup);
-  const modalWatchedBtn = document.querySelector('.modal-film__btn-watched');
-  checkAdd('watched', modalWatchedBtn.dataset.id, modalWatchedBtn);
-  const modalQueueBtn = document.querySelector('.modal-film__btn-queque');
-  checkAdd('queue', modalQueueBtn.dataset.id, modalQueueBtn);
 }
