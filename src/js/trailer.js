c8466d96145a04abc5859b8c38f35786485b72fe
D. Akelar <108392983+akelarka@users.noreply.github.com>
2023-01-07 23:35:40 +0200

    qw

--- src/js/trailer.js
+++ src/js/trailer.js
@@ -21,28 +21,18 @@ function handelClickToPoster(e) {
 
   fetchVideo(valueId)
     .then(data => {
-      console.log(data);
-      return (videoArray = data.results[0]);
+      trailerKey = data.results[0].key;
+      const instance =  `<iframe width="100%" height="100%" src="https://www.youtube.com/embed/${trailerKey}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+      updateModalContainer(instance);
     })
-    .then(toShowVideo)
     .catch(error => {
-      console.error();
+      console.error(error);
     });
 }
 
 function fetchVideo(valueId) {
   userFilms.setId(valueId);
   return userFilms.onSearchTrailerById(valueId);
-}
-
-function markupVideo(videoArray) {
-  const key = videoArray.key;
-  return `<iframe width="100%" height="100%" src="https://www.youtube.com/embed/${key}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
-}
-
-function toShowVideo() {
-  const markup = markupVideo(videoArray);
-  updateModalContainer(markup);
 }
 
 function closeModalVideoWindow() {
